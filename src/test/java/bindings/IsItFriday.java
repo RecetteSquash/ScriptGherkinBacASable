12a3ee60650cc856b50e6e110402e714d5fef6e4
Naailah Aly <naly@NX-DOMAIN.LAN>
2019-12-03 17:36:18 +0100

    2nd commit

--- src/test/java/bindings/IsItFriday.java
+++ src/test/java/bindings/IsItFriday.java
@@ -12,26 +12,25 @@ public class IsItFriday {
     private String actualAnswer;
 
     private static String isItFriday(String today) {
-        return null;
+        if (today != "Friday"){
+            return "Nope";
+        }
     }
 
     @Given("^today is Sunday$")
-    public void today_is_Sunday() throws Throwable {
+    public void today_is_Sunday()  {
         today = "Sunday";
-        throw new PendingException();
     }
 
     @When("^I ask whether it's Friday yet$")
-    public void i_ask_whether_it_s_Friday_yet() throws Throwable {
+    public void i_ask_whether_it_s_Friday_yet()  {
         actualAnswer = IsItFriday.isItFriday(today);
-        throw new PendingException();
     }
 
     @Then("^I should be told \"([^\"]*)\"$")
-    public void i_should_be_told(String expectedAnswer) throws Throwable {
+    public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
        System.out.println("FRIDAYYY");
-       throw new PendingException();
     }
 
   }
